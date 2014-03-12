d9d1ae0b9fab0c795c9a52a3a91d5d881a82ca67
Michael Rappazzo <michael.rappazzo@gtnexus.com>
2014-03-12 17:51:35 -0400

    Updated fix-git scripts

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -s ~/dots/.profile ~/.profile
 ln -s ~/dots/.vimrc ~/.vimrc
 ln -s ~/dots/.vim ~/.vim
 ln -s ~/dots/.profile ~/.bashrc
+ln -s ~/dots/.profile ~/.bash_login
+ln -s ~/dots/.profile ~/.bash_profile
