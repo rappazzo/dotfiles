88a34418fc7df6ee4bc7a368b1f31d6caa60af78
Michael Rappazzo <michael.rappazzo@gtnexus.com>
2015-07-30 08:51:07 -0400

    install.sh: re-enable linking profiles

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 here=$(dirname $0)
 
-#ln -s ${here}/.profile ~/.profile
+ln -s ${here}/.profile ~/.profile
 ln -s ${here}/.vimrc ~/.vimrc
 ln -s ${here}/.vim ~/.vim
-#ln -s ${here}/.profile ~/.bashrc
-#ln -s ${here}/.profile ~/.bash_login
-#ln -s ${here}/.profile ~/.bash_profile
-echo "Automatic profile linking disabled -- use zsh or bash"
+ln -s ${here}/.profile ~/.bashrc
+ln -s ${here}/.profile ~/.bash_login
+ln -s ${here}/.profile ~/.bash_profile
+ln -s ${here}/.zshrc_antigen ~/.zshrc
+ln -s ${here}/config ~/.config
