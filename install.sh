5580d08382731cac969df66ec0a4a7313b764762
Michael Rappazzo <rappazzo@gmail.com>
2018-04-27 14:45:25 -0400

    install.sh: fix path to vim, add config symlinks

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ here=$(dirname $0)
 
 ln -s ${here}/.profile ~/.profile
 ln -s ${here}/.vimrc ~/.vimrc
-ln -s ${here}/.vim ~/.vim
+ln -s ${here}/vim ~/.vim
 ln -s ${here}/.profile ~/.bashrc
 ln -s ${here}/.profile ~/.bash_login
 ln -s ${here}/.profile ~/.bash_profile
-ln -s ${here}/.zshrc_antigen ~/.zshrc
+ln -s ${here}/.zshrc ~/.zshrc
+ln -s ${here}/config/git/git ~/.gitconfig
+ln -s ${here}/config/git/global-gitignore .~/gitignore
 ln -s ${here}/config ~/.config
