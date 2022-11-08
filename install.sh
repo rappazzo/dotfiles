6ada0dad3f2e76777b78759079c1793e746f6661
Michael Rappazzo <michael.rappazzo@infor.com>
2022-11-08 13:45:17 -0500

    install.sh: delete the config files before install

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
 here=$(dirname $0)
+if [[ $here == "." ]]; then
+   here=$(pwd)
+fi
+
+rm ~/.profile
+rm ~/.vimrc
+rm ~/.vim
+rm ~/.bashrc
+rm ~/.bash_login
+rm ~/.bash_profile
+rm ~/.zshrc
+rm ~/.gitconfig
+rm -rf ~/.config
 
 ln -s ${here}/.profile ~/.profile
 ln -s ${here}/.vimrc ~/.vimrc
@@ -10,5 +23,4 @@ ln -s ${here}/.profile ~/.bash_login
 ln -s ${here}/.profile ~/.bash_profile
 ln -s ${here}/.zshrc ~/.zshrc
 ln -s ${here}/config/git/git ~/.gitconfig
-ln -s ${here}/config/git/global-gitignore .~/gitignore
 ln -s ${here}/config ~/.config
